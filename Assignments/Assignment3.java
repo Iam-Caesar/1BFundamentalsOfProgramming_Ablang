6fb491d254dd40fde32de7213ce5688c605f183b
Caesar <caesarenrique.ablang@icloud.com>
2026-09-02 21:05:49 +0800

    adding Assignment3.java

---
+++ Assignments/Assignment3.java
@@ -0,0 +1,16 @@
+public class Assignment3 {
+    public static void main(String[] args) { // main method
+        String firstname = "Caesar Enrique"; // assigns first name
+        String lastname = "Ablang"; // assigns last name
+        String address = "Manaoag"; // assigns municipality
+        String mobilenumber = "09399099602"; // assigns mobile number
+        int age = 24; // assigns age
+        double gwa = 94.72; // assigns sample GWA
+        System.out.println("Firstname: " + firstname); // prints first name
+        System.out.println("Lastname: " + lastname); // prints last name
+        System.out.println("address: " + address); // prints address
+        System.out.println("Mobile Number: " + mobilenumber); // prints mobile number
+        System.out.println("Age: " + age); // prints age
+        System.out.println("GWA: " + gwa); // prints sample GWA
+    } // close
+} // close
